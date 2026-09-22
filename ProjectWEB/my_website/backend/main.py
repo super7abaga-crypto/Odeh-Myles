8fba3dc76b65be441f634787721dc2d862af467c
odeh1 <super7abaga.com>
2026-09-22 12:00:40 +0100

    MyWebProject

--- ProjectWEB/my_website/backend/main.py
+++ ProjectWEB/my_website/backend/main.py
@@ -87,6 +87,25 @@ def get_users(db=Depends(get_db)):
 
     return users
 
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db=Depends(get_db)):
+    user = db.query(models.User).filter(
+        models.User.id == user_id
+    ).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    db.delete(user)
+    db.commit()
+
+    return {
+        "message": "User deleted successfully"
+    }
+
 @app.put("/users/{user_id}", response_model=UserResponse)
 def update_user(
     user_id: int,
--- ProjectWEB/my_website/frontend/app.js
+++ ProjectWEB/my_website/frontend/app.js
@@ -42,13 +42,60 @@ async function loadUsers() {
 
         userList.innerHTML = "";
 
-        users.forEach(function (user) {
-            const listItem = document.createElement("li");
+    users.forEach(function (user) {
+        const listItem = document.createElement("li");
 
-            listItem.textContent = `${user.name} - ${user.email}`;
+        listItem.textContent = `${user.name} - ${user.email} `;
 
-            userList.appendChild(listItem);
+        const editButton = document.createElement("button");
+
+editButton.textContent = "Edit";
+
+editButton.addEventListener("click", async function () {
+    const newName = prompt("Enter new name:", user.name);
+    const newEmail = prompt("Enter new email:", user.email);
+
+    if (newName === null || newEmail === null) {
+        return;
+    }
+
+    const response = await fetch(`http://127.0.0.1:8000/users/${user.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: newName,
+            email: newEmail
+        })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        loadUsers();
+    } else {
+        alert(data.detail);
+    }
+});
+
+        const deleteButton = document.createElement("button");
+
+        deleteButton.textContent = "Delete";
+
+        deleteButton.addEventListener("click", async function () {
+            await fetch(`http://127.0.0.1:8000/users/${user.id}`, {
+                method: "DELETE"
+            });
+
+            loadUsers();
         });
+
+        listItem.appendChild(editButton);
+        listItem.appendChild(deleteButton);
+
+        userList.appendChild(listItem);
+    });
 
     } catch (error) {
         userList.textContent = "Could not load users.";
